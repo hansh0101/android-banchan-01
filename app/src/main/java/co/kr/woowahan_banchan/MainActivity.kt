58bd6ce8803e073a493610ac1c40a363862b874d
hansh0101 <hansh0101@naver.com>
2022-08-08 21:25:31 +0900

    Initial commit

---
+++ app/src/main/java/co/kr/woowahan_banchan/MainActivity.kt
@@ -0,0 +1,11 @@
+package co.kr.woowahan_banchan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
