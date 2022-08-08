f2b14bd73033e60e4f77b92d6b70870b3ae91c60
hansh0101 <hansh0101@naver.com>
2022-08-08 22:00:40 +0900

    [CHORE] #1 - BaseClass 구현

---
+++ app/src/main/java/co/kr/woowahan_banchan/presentation/base/BaseActivity.kt
@@ -0,0 +1,18 @@
+package co.kr.woowahan_banchan.presentation.base
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<T: ViewDataBinding>: AppCompatActivity() {
+    protected lateinit var binding: T
+    abstract val layoutRes: Int
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, layoutRes)
+        binding.lifecycleOwner = this
+    }
+}
