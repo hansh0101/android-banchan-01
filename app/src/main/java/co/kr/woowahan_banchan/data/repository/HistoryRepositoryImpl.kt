9233f3401c93063a784d9bb9f0d89e4ef50c51c0
hansh0101 <hansh0101@naver.com>
2022-08-11 01:06:01 +0900

    [FEATURE] #18 - HistoryRepositoryImpl 구현

---
+++ app/src/main/java/co/kr/woowahan_banchan/data/repository/HistoryRepositoryImpl.kt
@@ -0,0 +1,47 @@
+package co.kr.woowahan_banchan.data.repository
+
+import co.kr.woowahan_banchan.data.datasource.local.cart.CartDataSource
+import co.kr.woowahan_banchan.data.datasource.local.history.HistoryDataSource
+import co.kr.woowahan_banchan.data.datasource.remote.detail.DetailDataSource
+import co.kr.woowahan_banchan.domain.entity.history.HistoryItem
+import co.kr.woowahan_banchan.domain.repository.HistoryRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class HistoryRepositoryImpl @Inject constructor(
+    private val historyDataSource: HistoryDataSource,
+    private val cartDataSource: CartDataSource,
+    private val detailDataSource: DetailDataSource,
+    private val coroutineDispatcher: CoroutineDispatcher
+) : HistoryRepository {
+    override fun getHistories(previewMode: Boolean): Flow<List<HistoryItem>> {
+        return combine(
+            historyDataSource.getItems(),
+            cartDataSource.getItems()
+        ) { historyDtoList, cartDtoList ->
+            Pair(historyDtoList, cartDtoList)
+        }.map { pair ->
+            val historyDtoList = pair.first
+            val cartDtoList = pair.second
+            historyDtoList.mapNotNull { historyDto ->
+                val apiResult = detailDataSource.getDetail(historyDto.hash)
+                when (apiResult.isSuccess) {
+                    true -> {
+                        apiResult.getOrThrow().data.toHistoryItem(
+                            historyDto.hash,
+                            historyDto.name,
+                            historyDto.time,
+                            cartDtoList.find { it.hash == historyDto.hash } != null
+                        )
+                    }
+                    false -> {
+                        null
+                    }
+                }
+            }
+        }
+    }
+}
