b3d555b427e4f261c0c2ea55d9ab070425046070
Skipancho <ctg1770a@gmail.com>
2022-08-10 22:07:02 +0900

    [FEATURE] #15 - getDishes 함수 추가

--- app/src/main/java/co/kr/woowahan_banchan/data/repository/DishRepositoryImpl.kt
+++ app/src/main/java/co/kr/woowahan_banchan/data/repository/DishRepositoryImpl.kt
@@ -2,9 +2,14 @@ package co.kr.woowahan_banchan.data.repository
 
 import co.kr.woowahan_banchan.data.datasource.local.cart.CartDataSource
 import co.kr.woowahan_banchan.data.datasource.remote.best.BestDataSource
+import co.kr.woowahan_banchan.data.datasource.remote.maindish.MainDishDataSource
+import co.kr.woowahan_banchan.data.datasource.remote.sidedish.SideDishDataSource
+import co.kr.woowahan_banchan.data.datasource.remote.soupdish.SoupDishDataSource
 import co.kr.woowahan_banchan.data.model.local.CartDto
 import co.kr.woowahan_banchan.domain.entity.dish.BestItem
+import co.kr.woowahan_banchan.domain.entity.dish.Dish
 import co.kr.woowahan_banchan.domain.repository.DishRepository
+import co.kr.woowahan_banchan.domain.repository.Source
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOn
@@ -15,6 +20,9 @@ import javax.inject.Inject
 class DishRepositoryImpl @Inject constructor(
     private val cartDataSource: CartDataSource,
     private val bestDataSource: BestDataSource,
+    private val mainDishDataSource: MainDishDataSource,
+    private val sideDishDataSource: SideDishDataSource,
+    private val soupDishDataSource: SoupDishDataSource,
     private val coroutineDispatcher: CoroutineDispatcher
 ) : DishRepository {
     override suspend fun addToCart(hash: String, amount: Int, name: String) {
@@ -24,8 +32,7 @@ class DishRepositoryImpl @Inject constructor(
     }
 
     override fun getBestDishes(): Flow<List<BestItem>> {
-        val cartFlow = cartDataSource.getItems()
-        return cartFlow.map { cartDtoList ->
+        return cartDataSource.getItems().map { cartDtoList ->
             val apiResult = bestDataSource.getBests()
             when (apiResult.isSuccess) {
                 true -> {
@@ -36,6 +43,26 @@ class DishRepositoryImpl @Inject constructor(
                                 bestDish.toEntity(cartDtoList.find { it.hash == bestDish.detailHash } != null)
                             }
                         )
+                    }
+                }
+                false -> {
+                    listOf()
+                }
+            }
+        }.flowOn(coroutineDispatcher)
+    }
+
+    override fun getDishes(source: Source): Flow<List<Dish>> {
+        return cartDataSource.getItems().map { cartDtoList ->
+            val apiResult = when (source) {
+                Source.MAIN -> mainDishDataSource.getMainDishes()
+                Source.SIDE -> sideDishDataSource.getSideDishes()
+                Source.SOUP -> soupDishDataSource.getSoupDishes()
+            }
+            when (apiResult.isSuccess) {
+                true -> {
+                    apiResult.getOrDefault(listOf()).map { dish ->
+                        dish.toEntity(cartDtoList.find { it.hash == dish.detailHash } != null)
                     }
                 }
                 false -> {
--- app/src/main/java/co/kr/woowahan_banchan/domain/repository/DishRepository.kt
+++ app/src/main/java/co/kr/woowahan_banchan/domain/repository/DishRepository.kt
@@ -1,9 +1,15 @@
 package co.kr.woowahan_banchan.domain.repository
 
 import co.kr.woowahan_banchan.domain.entity.dish.BestItem
+import co.kr.woowahan_banchan.domain.entity.dish.Dish
 import kotlinx.coroutines.flow.Flow
 
 interface DishRepository {
     suspend fun addToCart(hash: String, amount: Int, name: String)
     fun getBestDishes(): Flow<List<BestItem>>
+    fun getDishes(source: Source): Flow<List<Dish>>
+}
+
+enum class Source {
+    MAIN, SIDE, SOUP
 }
