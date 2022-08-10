c3ebd0844ca0bae99390e4f6dea8d54604b9dc16
hansh0101 <hansh0101@naver.com>
2022-08-10 20:57:52 +0900

    [FEATURE] #15 - flowOn(DefaultDispatcher) 메서드 추가

--- app/src/main/java/co/kr/woowahan_banchan/data/repository/DishRepositoryImpl.kt
+++ app/src/main/java/co/kr/woowahan_banchan/data/repository/DishRepositoryImpl.kt
@@ -5,14 +5,17 @@ import co.kr.woowahan_banchan.data.datasource.remote.best.BestDataSource
 import co.kr.woowahan_banchan.data.model.local.CartDto
 import co.kr.woowahan_banchan.domain.entity.dish.BestItem
 import co.kr.woowahan_banchan.domain.repository.DishRepository
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 import java.util.*
 import javax.inject.Inject
 
 class DishRepositoryImpl @Inject constructor(
     private val cartDataSource: CartDataSource,
-    private val bestDataSource: BestDataSource
+    private val bestDataSource: BestDataSource,
+    private val coroutineDispatcher: CoroutineDispatcher
 ) : DishRepository {
     override suspend fun addToCart(hash: String, amount: Int, name: String) {
         cartDataSource.insertOrUpdateItems(
@@ -39,6 +42,6 @@ class DishRepositoryImpl @Inject constructor(
                     listOf()
                 }
             }
-        }
+        }.flowOn(coroutineDispatcher)
     }
 }
