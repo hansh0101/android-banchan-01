fa9508e280f93e267f3a9006fc8a5def821a5edd
hansh0101 <hansh0101@naver.com>
2022-08-11 14:42:46 +0900

    [FEATURE] #26 - OrderHistoryRepository 인터페이스 생성

---
+++ app/src/main/java/co/kr/woowahan_banchan/domain/repository/OrderHistoryRepository.kt
@@ -0,0 +1,9 @@
+package co.kr.woowahan_banchan.domain.repository
+
+import co.kr.woowahan_banchan.domain.entity.orderhistory.OrderHistory
+import co.kr.woowahan_banchan.domain.entity.orderhistory.OrderItem
+
+interface OrderHistoryRepository {
+    suspend fun getOrderHistories(): List<OrderHistory>
+    suspend fun getOrderReceipt(orderId: Long): List<OrderItem>?
+}
