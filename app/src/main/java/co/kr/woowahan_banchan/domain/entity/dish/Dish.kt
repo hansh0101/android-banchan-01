df97ae43ba12f80952bd643cca833f620a50094a
Skipancho <ctg1770a@gmail.com>
2022-08-11 20:49:55 +0900

    [FEATURE] #33 - DishAdapter, BestItemAdapter 구현

--- app/src/main/java/co/kr/woowahan_banchan/domain/entity/dish/Dish.kt
+++ app/src/main/java/co/kr/woowahan_banchan/domain/entity/dish/Dish.kt
@@ -1,5 +1,7 @@
 package co.kr.woowahan_banchan.domain.entity.dish
 
+import java.text.NumberFormat
+
 data class Dish(
     val detailHash: String,
     val imageUrl: String,
@@ -12,4 +14,14 @@ data class Dish(
     val sPrice: Int,
     val badge: List<String>? = null,
     val isAdded: Boolean
-)
+) {
+    val nPriceText: String
+        get() =
+            if (nPrice != null)
+                "${NumberFormat.getNumberInstance().format(nPrice)}원"
+            else
+                ""
+
+    val sPriceText: String
+        get() = "${NumberFormat.getNumberInstance().format(sPrice)}원"
+}
