33480df1ec9dcc283021a802611058e9b3d19d96
hansh0101 <hansh0101@naver.com>
2022-08-11 14:42:26 +0900

    [FEATURE] #26 - 주문내역 data class 생성

---
+++ app/src/main/java/co/kr/woowahan_banchan/domain/entity/orderhistory/OrderHistory.kt
@@ -0,0 +1,10 @@
+package co.kr.woowahan_banchan.domain.entity.orderhistory
+
+data class OrderHistory(
+    val orderId: Long,
+    val thumbnailUrl: String,
+    val title: String,
+    val count: Int,
+    val totalPrice: Int,
+    val time: Long
+)
