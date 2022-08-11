bfb4261d50b6fa6e74eab555780bea895fa1e91d
hansh0101 <hansh0101@naver.com>
2022-08-11 14:42:02 +0900

    [FEATURE] #26 - Flow 관련 import 제거

--- app/src/main/java/co/kr/woowahan_banchan/data/datasource/local/order/OrderDataSourceImpl.kt
+++ app/src/main/java/co/kr/woowahan_banchan/data/datasource/local/order/OrderDataSourceImpl.kt
@@ -3,11 +3,7 @@ package co.kr.woowahan_banchan.data.datasource.local.order
 import co.kr.woowahan_banchan.data.database.dao.OrderDao
 import co.kr.woowahan_banchan.data.model.local.OrderDto
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.withContext
-import timber.log.Timber
 import javax.inject.Inject
 
 class OrderDataSourceImpl @Inject constructor(
--- app/src/main/java/co/kr/woowahan_banchan/data/datasource/local/orderitem/OrderItemDataSourceImpl.kt
+++ app/src/main/java/co/kr/woowahan_banchan/data/datasource/local/orderitem/OrderItemDataSourceImpl.kt
@@ -3,12 +3,7 @@ package co.kr.woowahan_banchan.data.datasource.local.orderitem
 import co.kr.woowahan_banchan.data.database.dao.OrderItemDao
 import co.kr.woowahan_banchan.data.model.local.OrderItemDto
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.withContext
-import timber.log.Timber
 import javax.inject.Inject
 
 class OrderItemDataSourceImpl @Inject constructor(
