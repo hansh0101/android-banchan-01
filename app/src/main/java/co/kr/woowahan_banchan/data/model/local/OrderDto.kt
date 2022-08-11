744792c37cb933b373a92589b9ad5df66b79345f
hansh0101 <hansh0101@naver.com>
2022-08-11 14:41:18 +0900

    [FEATURE] #26 - OrderHistory를 리턴하는 확장함수 구현

--- app/src/main/java/co/kr/woowahan_banchan/data/model/local/OrderDto.kt
+++ app/src/main/java/co/kr/woowahan_banchan/data/model/local/OrderDto.kt
@@ -3,10 +3,22 @@ package co.kr.woowahan_banchan.data.model.local
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import co.kr.woowahan_banchan.domain.entity.orderhistory.OrderHistory
 
 @Entity(tableName = "ORDER")
 data class OrderDto(
     @PrimaryKey(autoGenerate = true) val id : Long = 0,
     @ColumnInfo(name = "total_price") val totalPrice : Int,
     val time : Long
-)
+) {
+    fun toOrderHistory(thumbnailUrl: String, title: String, count: Int): OrderHistory {
+        return OrderHistory(
+            this.id,
+            thumbnailUrl,
+            title,
+            count,
+            this.totalPrice,
+            this.time
+        )
+    }
+}
