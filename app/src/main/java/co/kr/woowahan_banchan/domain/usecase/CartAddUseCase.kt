dc8e8cf9fdf7181fbc3edfc43930e36e9435c480
hansh0101 <hansh0101@naver.com>
2022-08-12 19:43:48 +0900

    [FEATURE] #42 - 장바구니 추가 UseCase 구현

---
+++ app/src/main/java/co/kr/woowahan_banchan/domain/usecase/CartAddUseCase.kt
@@ -0,0 +1,13 @@
+package co.kr.woowahan_banchan.domain.usecase
+
+import co.kr.woowahan_banchan.domain.repository.CartRepository
+import javax.inject.Inject
+
+class CartAddUseCase @Inject constructor(
+    private val cartRepository: CartRepository
+) {
+    suspend operator fun invoke(hash: String, amount: Int, name: String): Result<Unit> =
+        runCatching {
+            cartRepository.addToCart(hash, amount, name)
+        }
+}
