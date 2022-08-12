95c94a8be47bb9eae1e1543e996771dc5c3bbed2
hansh0101 <hansh0101@naver.com>
2022-08-13 01:29:25 +0900

    [FEATURE] #49 - OrderActivity initView(), setToolbar() 메서드 구현

---
+++ app/src/main/java/co/kr/woowahan_banchan/presentation/ui/order/OrderActivity.kt
@@ -0,0 +1,40 @@
+package co.kr.woowahan_banchan.presentation.ui.order
+
+import android.os.Bundle
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import co.kr.woowahan_banchan.R
+import co.kr.woowahan_banchan.databinding.ActivityOrderBinding
+import co.kr.woowahan_banchan.presentation.ui.base.BaseActivity
+import co.kr.woowahan_banchan.presentation.ui.order.orderlist.OrderListFragment
+
+class OrderActivity : BaseActivity<ActivityOrderBinding>() {
+    override val layoutRes: Int
+        get() = R.layout.activity_order
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initView()
+    }
+
+    private fun initView() {
+        supportFragmentManager.commit {
+            add<OrderListFragment>(R.id.fcv_order)
+        }
+    }
+
+    fun setToolbar(fragmentType: FragmentType) {
+        when(fragmentType) {
+            FragmentType.ORDER_LIST -> {
+                binding.tbToolbar.title = "OrderList"
+                binding.tbToolbar.setNavigationIcon(R.drawable.ic_arrow_left)
+            }
+        }
+    }
+
+    companion object {
+        enum class FragmentType {
+            ORDER_LIST
+        }
+    }
+}
