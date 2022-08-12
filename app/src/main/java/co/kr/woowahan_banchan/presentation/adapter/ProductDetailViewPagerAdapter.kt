da08906faf6a77fa21bc1f5a4fcfb6918fc6d487
hansh0101 <hansh0101@naver.com>
2022-08-12 11:16:59 +0900

    [FEATURE] #30 - ProductDetailViewPagerAdapter 구현

---
+++ app/src/main/java/co/kr/woowahan_banchan/presentation/adapter/ProductDetailViewPagerAdapter.kt
@@ -0,0 +1,52 @@
+package co.kr.woowahan_banchan.presentation.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.RecyclerView
+import co.kr.woowahan_banchan.databinding.ItemProductDetailBinding
+import co.kr.woowahan_banchan.util.ImageLoader
+
+class ProductDetailViewPagerAdapter :
+    RecyclerView.Adapter<ProductDetailViewPagerAdapter.ImageViewHolder>() {
+    private var items: List<String> = listOf()
+
+    class ImageViewHolder(private val binding: ItemProductDetailBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun onBind(imageUrl: String) {
+            ImageLoader.loadImage(imageUrl) {
+                if (it == null) {
+                    binding.progressBar.isVisible = true
+                    binding.ivProductDetail.isVisible = false
+                } else {
+                    binding.progressBar.isVisible = false
+                    binding.ivProductDetail.setImageBitmap(it)
+                }
+            }
+        }
+
+        companion object {
+            fun create(parent: ViewGroup): ImageViewHolder {
+                val binding = ItemProductDetailBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+                return ImageViewHolder(binding)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder =
+        ImageViewHolder.create(parent)
+
+    override fun onBindViewHolder(holder: ImageViewHolder, position: Int) =
+        holder.onBind(items[position])
+
+    override fun getItemCount(): Int = items.size
+
+    fun updateItems(newItems: List<String>) {
+        items = newItems
+        notifyDataSetChanged()
+    }
+}
