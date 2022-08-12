cffe5bc4e5a4366ebadcf684eae3fa0e065e3e5a
hansh0101 <hansh0101@naver.com>
2022-08-12 19:45:28 +0900

    [FEATURE] #42 - 장바구니 추가 기능 구현

--- app/src/main/java/co/kr/woowahan_banchan/presentation/viewmodel/productdetail/ProductDetailViewModel.kt
+++ app/src/main/java/co/kr/woowahan_banchan/presentation/viewmodel/productdetail/ProductDetailViewModel.kt
@@ -3,27 +3,41 @@ package co.kr.woowahan_banchan.presentation.viewmodel.productdetail
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import co.kr.woowahan_banchan.domain.entity.detail.DishInfo
+import co.kr.woowahan_banchan.domain.usecase.CartAddUseCase
 import co.kr.woowahan_banchan.domain.usecase.ProductDetailUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ProductDetailViewModel @Inject constructor(
-    private val productDetailUseCase: ProductDetailUseCase
+    private val productDetailUseCase: ProductDetailUseCase,
+    private val cartAddUseCase: CartAddUseCase
 ) : ViewModel() {
-    private val _dishInfo = MutableStateFlow<UiState>(UiState.Init)
-    val dishInfo: StateFlow<UiState> get() = _dishInfo
+    private val _dishInfo = MutableStateFlow<UiState<DishInfo>>(UiState.Init)
+    val dishInfo: StateFlow<UiState<DishInfo>> get() = _dishInfo
     private val _amount = MutableStateFlow<Int>(1)
     val amount: StateFlow<Int> get() = _amount
+    private val _isAddSuccess = MutableSharedFlow<Boolean>()
+    val isAddSuccess: SharedFlow<Boolean> get() = _isAddSuccess
 
     fun fetchUiState(hash: String) {
         viewModelScope.launch {
             productDetailUseCase(hash)
                 .onSuccess { _dishInfo.value = UiState.Success(it) }
                 .onFailure { _dishInfo.value = UiState.Error(it.message) }
+        }
+    }
+
+    fun addToCart(hash: String, name: String) {
+        viewModelScope.launch {
+            cartAddUseCase(hash, amount.value, name)
+                .onSuccess { _isAddSuccess.emit(true) }
+                .onFailure { _isAddSuccess.emit(false) }
         }
     }
 
@@ -37,9 +51,9 @@ class ProductDetailViewModel @Inject constructor(
         }
     }
 
-    sealed class UiState {
-        object Init : UiState()
-        data class Success(val dishInfo: DishInfo) : UiState()
-        data class Error(val message: String?) : UiState()
+    sealed class UiState<out T> {
+        object Init : UiState<Nothing>()
+        data class Success<out T>(val data: T) : UiState<T>()
+        data class Error(val message: String?) : UiState<Nothing>()
     }
 }
