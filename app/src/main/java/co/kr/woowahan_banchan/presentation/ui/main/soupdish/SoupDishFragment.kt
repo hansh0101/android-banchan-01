6673f1d96b29ec2b01faf8d4094e63e910b55fac
Skipancho <ctg1770a@gmail.com>
2022-08-13 21:14:15 +0900

    [FEATURE] #56 - Soup 화면 UI 구현

--- app/src/main/java/co/kr/woowahan_banchan/presentation/ui/main/soupdish/SoupDishFragment.kt
+++ app/src/main/java/co/kr/woowahan_banchan/presentation/ui/main/soupdish/SoupDishFragment.kt
@@ -1,10 +1,115 @@
 package co.kr.woowahan_banchan.presentation.ui.main.soupdish
 
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.GridLayoutManager
 import co.kr.woowahan_banchan.R
 import co.kr.woowahan_banchan.databinding.FragmentSoupDishBinding
+import co.kr.woowahan_banchan.domain.repository.Source
+import co.kr.woowahan_banchan.presentation.adapter.DishAdapter
+import co.kr.woowahan_banchan.presentation.adapter.FilterSpinnerAdapter
+import co.kr.woowahan_banchan.presentation.decoration.GridItemDecoration
 import co.kr.woowahan_banchan.presentation.ui.base.BaseFragment
+import co.kr.woowahan_banchan.presentation.ui.productdetail.ProductDetailActivity
+import co.kr.woowahan_banchan.presentation.viewmodel.UiState
+import co.kr.woowahan_banchan.presentation.viewmodel.main.SoupDishViewModel
+import co.kr.woowahan_banchan.util.dpToPx
+import co.kr.woowahan_banchan.util.shortToast
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
+@AndroidEntryPoint
 class SoupDishFragment : BaseFragment<FragmentSoupDishBinding>() {
     override val layoutRes: Int
         get() = R.layout.fragment_soup_dish
+
+    private val viewModel by viewModels<SoupDishViewModel>()
+
+    private val dishAdapter by lazy {
+        DishAdapter { title, hash ->
+            startDetailActivity(
+                title,
+                hash
+            )
+        }
+    }
+
+    private val spinnerItems = listOf("기본 정렬순", "금액 높은순", "금액 낮은순", "할인율순")
+
+    private val filterAdapter by lazy { FilterSpinnerAdapter(requireContext()) }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initView()
+        observeData()
+        setListener()
+
+        viewModel.getDishes(Source.SOUP)
+    }
+
+    private fun initView() {
+        binding.rvSoupdishes.adapter = dishAdapter
+        binding.rvSoupdishes.layoutManager = GridLayoutManager(requireContext(), 2)
+        binding.rvSoupdishes.addItemDecoration(
+            GridItemDecoration(30.dpToPx(),16.dpToPx())
+        )
+        binding.rvSoupdishes.itemAnimator = null
+
+        binding.spFilter.adapter = filterAdapter
+        filterAdapter.submitList(spinnerItems, 0)
+    }
+
+    private fun observeData() {
+        viewModel.soupDishes.flowWithLifecycle(viewLifecycleOwner.lifecycle)
+            .onEach {
+                when (it) {
+                    is UiState.Init -> {
+                        binding.pbLoading.visibility = View.VISIBLE
+                    }
+                    is UiState.Success -> {
+                        viewModel.setDefaultMainDishes(it.data)
+                        viewModel.setSortedDishes(binding.spFilter.selectedItemPosition)
+                        binding.pbLoading.visibility = View.GONE
+                    }
+                    is UiState.Error -> {
+                        binding.pbLoading.visibility = View.GONE
+                        requireContext().shortToast(it.message)
+                    }
+                }
+            }.launchIn(lifecycleScope)
+
+        viewModel.sortedDishes.observe(viewLifecycleOwner) {
+            dishAdapter.submitList(it.toMutableList())
+        }
+    }
+
+    private fun setListener() {
+        binding.spFilter.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                adapterView: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                viewModel.setSortedDishes(position)
+                filterAdapter.submitList(
+                    spinnerItems, position
+                )
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+        }
+    }
+
+    private fun startDetailActivity(title: String, hash: String) {
+        startActivity(ProductDetailActivity.getIntent(requireContext(), title, hash))
+    }
 }
---
+++ app/src/main/java/co/kr/woowahan_banchan/presentation/viewmodel/main/SoupDishViewModel.kt
@@ -0,0 +1,50 @@
+package co.kr.woowahan_banchan.presentation.viewmodel.main
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import co.kr.woowahan_banchan.domain.entity.dish.Dish
+import co.kr.woowahan_banchan.domain.repository.Source
+import co.kr.woowahan_banchan.domain.usecase.GetDishesUseCase
+import co.kr.woowahan_banchan.presentation.viewmodel.UiState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SoupDishViewModel @Inject constructor(
+    private val getDishesUseCase: GetDishesUseCase
+) : ViewModel() {
+    private val _soupDishes = MutableStateFlow<UiState<List<Dish>>>(UiState.Init)
+    val soupDishes: StateFlow<UiState<List<Dish>>> get() = _soupDishes
+
+    private var defaultMainDishes = listOf<Dish>()
+
+    private val _sortedDishes = MutableLiveData<List<Dish>>(listOf())
+    val sortedDishes: LiveData<List<Dish>> get() = _sortedDishes
+
+    fun getDishes(source: Source) = viewModelScope.launch {
+        getDishesUseCase(source)
+            .catch { _soupDishes.value = UiState.Error("상품을 불러오는 것에 실패하였습니다.") }
+            .collect {
+                _soupDishes.value = UiState.Success(it)
+            }
+    }
+
+    fun setSortedDishes(sortType: Int) {
+        when (sortType) {
+            0 -> _sortedDishes.value = defaultMainDishes
+            1 -> _sortedDishes.value = defaultMainDishes.sortedByDescending { it.sPrice }
+            2 -> _sortedDishes.value = defaultMainDishes.sortedBy { it.sPrice }
+            3 -> _sortedDishes.value = defaultMainDishes.sortedByDescending { it.discount }
+        }
+    }
+
+    fun setDefaultMainDishes(list: List<Dish>) {
+        defaultMainDishes = list
+    }
+}
