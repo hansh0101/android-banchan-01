8b9df0cf2316e586bf3eb8f4fad613997e6b4ac9
hansh0101 <hansh0101@naver.com>
2022-08-13 17:31:09 +0900

    [FEATURE] #49 - dpToPx 확장함수 추가

--- app/src/main/java/co/kr/woowahan_banchan/util/NumberExtension.kt
+++ app/src/main/java/co/kr/woowahan_banchan/util/NumberExtension.kt
@@ -1,5 +1,7 @@
 package co.kr.woowahan_banchan.util
 
-fun Int.toPriceFormat(): String = String.format("%,2d", this)
+import android.content.res.Resources
 
+fun Int.toPriceFormat(): String = String.format("%,2d", this)
+fun Int.dpToPx(): Int = this * Resources.getSystem().displayMetrics.density.toInt()
 fun Long.calculateDiffToMinute(time: Long): Int = ((this - time) / 60000).toInt()
