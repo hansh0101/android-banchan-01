c57c64eec5d790690511edaef674492328ef7dbb
Skipancho <ctg1770a@gmail.com>
2022-08-14 22:39:44 +0900

    [FEATURE] #69 - 장바구니 아이콘 뱃지 구현

--- app/src/main/java/co/kr/woowahan_banchan/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/co/kr/woowahan_banchan/presentation/ui/main/MainActivity.kt
@@ -3,10 +3,16 @@ package co.kr.woowahan_banchan.presentation.ui.main
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.activity.viewModels
 import co.kr.woowahan_banchan.R
 import co.kr.woowahan_banchan.databinding.ActivityMainBinding
 import co.kr.woowahan_banchan.presentation.adapter.ViewPagerAdapter
 import co.kr.woowahan_banchan.presentation.ui.base.BaseActivity
+import co.kr.woowahan_banchan.presentation.viewmodel.MainViewModel
+import co.kr.woowahan_banchan.util.dpToPx
+import com.google.android.material.badge.BadgeDrawable
+import com.google.android.material.badge.BadgeUtils
+import com.google.android.material.badge.ExperimentalBadgeUtils
 import com.google.android.material.tabs.TabLayoutMediator
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,6 +21,8 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
     override val layoutRes: Int
         get() = R.layout.activity_main
 
+    private val viewModel by viewModels<MainViewModel>()
+
     private val tabTitleArray = arrayOf(
         "기획전",
         "든든한 메인요리",
@@ -22,11 +30,22 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
         "정갈한 밑반찬"
     )
 
+    private val badge by lazy {
+        BadgeDrawable.create(this).also { bd ->
+            bd.backgroundColor = resources.getColor(R.color.grayscale_000000,theme)
+            bd.horizontalOffset = 5.dpToPx()
+            bd.verticalOffset = 5.dpToPx()
+            bd.maxCharacterCount = 3
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         initToolbar()
         initView()
+        observeData()
+
+        viewModel.getCartItemCount()
     }
 
     private fun initToolbar(){
@@ -43,8 +62,17 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
         }
     }
 
+    private fun observeData(){
+        viewModel.cartCount.observe(this){
+            badge.number = it
+            badge.isVisible = it > 0
+        }
+    }
+
+    @ExperimentalBadgeUtils
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_app_bar,menu)
+        BadgeUtils.attachBadgeDrawable(badge,binding.tbToolbar,R.id.action_cart)
         return super.onCreateOptionsMenu(menu)
     }
 
--- app/src/main/java/co/kr/woowahan_banchan/presentation/viewmodel/MainViewModel.kt
+++ app/src/main/java/co/kr/woowahan_banchan/presentation/viewmodel/MainViewModel.kt
@@ -1,12 +1,28 @@
 package co.kr.woowahan_banchan.presentation.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import co.kr.woowahan_banchan.domain.usecase.GetCartItemCountUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor() : ViewModel() {
+class MainViewModel @Inject constructor(
+    private val getCartItemCountUseCase: GetCartItemCountUseCase
+) : ViewModel() {
 
+    private val _cartCount = MutableLiveData(0)
+    val cartCount: LiveData<Int> get() = _cartCount
+
+    fun getCartItemCount() = viewModelScope.launch {
+        getCartItemCountUseCase()
+            .catch { _cartCount.value = 0 }
+            .collect {
+                _cartCount.value = it
+            }
+    }
 }
-
-
