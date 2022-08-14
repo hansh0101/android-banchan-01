a834ec950df2d45271e9c0d019458d24c9927996
Skipancho <ctg1770a@gmail.com>
2022-08-14 15:43:21 +0900

    [FEATURE] #66 - 장바구니 담기 이벤트 적용

--- app/src/main/java/co/kr/woowahan_banchan/presentation/viewmodel/bottomsheet/BottomSheetViewModel.kt
+++ app/src/main/java/co/kr/woowahan_banchan/presentation/viewmodel/bottomsheet/BottomSheetViewModel.kt
@@ -3,13 +3,21 @@ package co.kr.woowahan_banchan.presentation.viewmodel.bottomsheet
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import co.kr.woowahan_banchan.domain.entity.dish.Dish
+import co.kr.woowahan_banchan.domain.usecase.CartAddUseCase
+import co.kr.woowahan_banchan.presentation.viewmodel.UiState
 import co.kr.woowahan_banchan.util.toPriceFormat
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class BottomSheetViewModel @Inject constructor() : ViewModel(){
+class BottomSheetViewModel @Inject constructor(
+    private val cartAddUseCase: CartAddUseCase
+) : ViewModel(){
     private val _currentDish = MutableLiveData<Dish>()
     val currentDish : LiveData<Dish> get() = _currentDish
 
@@ -19,6 +27,8 @@ class BottomSheetViewModel @Inject constructor() : ViewModel(){
     private val _totalPriceText = MutableLiveData("0원")
     val totalPriceText : LiveData<String> get() = _totalPriceText
 
+    private val _isAddSuccess = MutableStateFlow<UiState<Unit>>(UiState.Init)
+    val isAddSuccess : StateFlow<UiState<Unit>> get() = _isAddSuccess
 
     fun setCurrentDish(dish : Dish) {
         _currentDish.value = dish
@@ -36,5 +46,15 @@ class BottomSheetViewModel @Inject constructor() : ViewModel(){
         }
         val salePrice = currentDish.value?.sPrice ?: return
         _totalPriceText.value = "${(salePrice * currentAmount).toPriceFormat()}원"
+    }
+
+    fun addToCart() {
+        val dish = currentDish.value ?: return
+        val currentAmount = amount.value ?: return
+        viewModelScope.launch {
+            cartAddUseCase(dish.detailHash,currentAmount,dish.title)
+                .onSuccess { _isAddSuccess.value = UiState.Success(it) }
+                .onFailure { _isAddSuccess.value = UiState.Error("장바구니 추가에 실패하였습니다.") }
+        }
     }
 }
