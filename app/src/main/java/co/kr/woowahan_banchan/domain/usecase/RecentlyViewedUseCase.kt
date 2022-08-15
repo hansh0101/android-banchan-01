d1b1cc2532fada88d7bd2cebd04b7e78a975c693
hansh0101 <hansh0101@naver.com>
2022-08-15 19:42:51 +0900

    [FEATURE] #75 - 최근 조회 메뉴 UseCase 구현

---
+++ app/src/main/java/co/kr/woowahan_banchan/domain/usecase/RecentlyViewedUseCase.kt
@@ -0,0 +1,13 @@
+package co.kr.woowahan_banchan.domain.usecase
+
+import co.kr.woowahan_banchan.domain.entity.history.HistoryItem
+import co.kr.woowahan_banchan.domain.repository.HistoryRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class RecentlyViewedUseCase @Inject constructor(
+    private val historyRepository: HistoryRepository
+) {
+    operator fun invoke(previewMode: Boolean): Flow<List<HistoryItem>> =
+        historyRepository.getHistories(previewMode)
+}
