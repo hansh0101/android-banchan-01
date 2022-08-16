940055b99b31907317ccff8868805a6a25331188
hansh0101 <hansh0101@naver.com>
2022-08-16 15:45:49 +0900

    [FEATURE] #81 - 가장 최근 주문의 주문 시간을 조회하는 ViewModel 메서드, StateFlow 변수 구현

--- app/src/main/java/co/kr/woowahan_banchan/presentation/viewmodel/MainViewModel.kt
+++ app/src/main/java/co/kr/woowahan_banchan/presentation/viewmodel/MainViewModel.kt
@@ -3,20 +3,27 @@ package co.kr.woowahan_banchan.presentation.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import co.kr.woowahan_banchan.domain.usecase.GetCartItemCountUseCase
+import co.kr.woowahan_banchan.domain.usecase.LatestOrderTimeUseCase
+import co.kr.woowahan_banchan.util.calculateDiffToMinute
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
+import java.util.*
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    private val getCartItemCountUseCase: GetCartItemCountUseCase
+    private val getCartItemCountUseCase: GetCartItemCountUseCase,
+    private val latestOrderTimeUseCase: LatestOrderTimeUseCase
 ) : ViewModel() {
 
     private val _cartCount = MutableStateFlow(0)
     val cartCount: StateFlow<Int> get() = _cartCount
+
+    private val _isOrderCompleted = MutableStateFlow<Boolean>(true)
+    val isOrderCompleted: StateFlow<Boolean> get() = _isOrderCompleted
 
     fun getCartItemCount() = viewModelScope.launch {
         getCartItemCountUseCase()
@@ -24,5 +31,13 @@ class MainViewModel @Inject constructor(
             .collect {
                 _cartCount.value = it
             }
+    }
+
+    fun fetchLatestOrderTime() {
+        viewModelScope.launch {
+            latestOrderTimeUseCase().collect { time ->
+                _isOrderCompleted.value = Date().time.calculateDiffToMinute(time) >= 20
+            }
+        }
     }
 }
