96d0ccbeba5b1d8fb12faf8dc1fdfcb8442efc8d
hansh0101 <hansh0101@naver.com>
2022-08-16 15:49:15 +0900

    [DEL] #81 - Success에 불필요하게 선언된 out 키워드 제거

--- app/src/main/java/co/kr/woowahan_banchan/presentation/viewmodel/UiState.kt
+++ app/src/main/java/co/kr/woowahan_banchan/presentation/viewmodel/UiState.kt
@@ -2,6 +2,6 @@ package co.kr.woowahan_banchan.presentation.viewmodel
 
 sealed class UiState<out T> {
     object Init : UiState<Nothing>()
-    data class Success<out T>(val data: T) : UiState<T>()
+    data class Success<T>(val data: T) : UiState<T>()
     data class Error(val message: String?) : UiState<Nothing>()
 }
