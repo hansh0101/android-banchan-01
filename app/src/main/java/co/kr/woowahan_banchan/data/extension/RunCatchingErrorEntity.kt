e9c7dcc48c72574faf54801b72febe9fc4948404
hansh0101 <hansh0101@naver.com>
2022-08-20 21:03:02 +0900

    [FEATURE] #105 - failure 시 ErrorEntity를 담아 Result.failure를 내보내는 runCatchingErrorEntity 메서드 구현

---
+++ app/src/main/java/co/kr/woowahan_banchan/data/extension/RunCatchingErrorEntity.kt
@@ -0,0 +1,30 @@
+package co.kr.woowahan_banchan.data.extension
+
+import co.kr.woowahan_banchan.domain.entity.error.ErrorEntity
+import retrofit2.HttpException
+import java.io.IOException
+import java.io.InterruptedIOException
+import java.net.HttpRetryException
+import java.net.HttpURLConnection
+import java.net.UnknownHostException
+import java.nio.channels.InterruptedByTimeoutException
+
+inline fun <T, R> T.runCatchingErrorEntity(block: T.() -> R): Result<R> {
+    return try {
+        Result.success(block())
+    } catch (e: Throwable) {
+        val error = when (e) {
+            is HttpRetryException, is InterruptedIOException, is InterruptedByTimeoutException -> ErrorEntity.RetryableError
+            is UnknownHostException -> ErrorEntity.ConditionalError
+            is IOException -> ErrorEntity.UnknownError
+            is HttpException -> {
+                when (e.code()) {
+                    HttpURLConnection.HTTP_CLIENT_TIMEOUT, HttpURLConnection.HTTP_GATEWAY_TIMEOUT -> ErrorEntity.RetryableError
+                    else -> ErrorEntity.UnknownError
+                }
+            }
+            else -> ErrorEntity.UnknownError
+        }
+        Result.failure(error)
+    }
+}
