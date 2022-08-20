f059f375c1b50e369a08337b1bbf1171096a0646
hansh0101 <hansh0101@naver.com>
2022-08-21 02:40:43 +0900

    [FEATURE] #105 - UiEvent로 처리하도록 변경

--- app/src/main/java/co/kr/woowahan_banchan/presentation/viewmodel/bottomsheet/BottomSheetViewModel.kt
+++ app/src/main/java/co/kr/woowahan_banchan/presentation/viewmodel/bottomsheet/BottomSheetViewModel.kt
@@ -5,32 +5,33 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import co.kr.woowahan_banchan.domain.entity.dish.SelectedDish
+import co.kr.woowahan_banchan.domain.entity.error.ErrorEntity
 import co.kr.woowahan_banchan.domain.usecase.CartAddUseCase
-import co.kr.woowahan_banchan.presentation.viewmodel.UiState
+import co.kr.woowahan_banchan.presentation.viewmodel.UiEvents
 import co.kr.woowahan_banchan.util.toPriceFormat
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class BottomSheetViewModel @Inject constructor(
     private val cartAddUseCase: CartAddUseCase
-) : ViewModel(){
+) : ViewModel() {
     private val _currentDish = MutableLiveData<SelectedDish>()
-    val currentDish : LiveData<SelectedDish> get() = _currentDish
+    val currentDish: LiveData<SelectedDish> get() = _currentDish
 
     private val _amount = MutableLiveData(1)
-    val amount : LiveData<Int> get() = _amount
+    val amount: LiveData<Int> get() = _amount
 
     private val _totalPriceText = MutableLiveData("0원")
-    val totalPriceText : LiveData<String> get() = _totalPriceText
+    val totalPriceText: LiveData<String> get() = _totalPriceText
 
-    private val _isAddSuccess = MutableStateFlow<UiState<Unit>>(UiState.Init)
-    val isAddSuccess : StateFlow<UiState<Unit>> get() = _isAddSuccess
+    private val _isAddSuccess = MutableSharedFlow<UiEvents<Unit>>()
+    val isAddSuccess: SharedFlow<UiEvents<Unit>> get() = _isAddSuccess
 
-    fun setCurrentDish(dish : SelectedDish) {
+    fun setCurrentDish(dish: SelectedDish) {
         _currentDish.value = dish
         _totalPriceText.value = dish.sPriceText
     }
@@ -52,9 +53,11 @@ class BottomSheetViewModel @Inject constructor(
         val dish = currentDish.value ?: return
         val currentAmount = amount.value ?: return
         viewModelScope.launch {
-            cartAddUseCase(dish.hash,currentAmount,dish.title)
-                .onSuccess { _isAddSuccess.value = UiState.Success(it) }
-                .onFailure { _isAddSuccess.value = UiState.Error("장바구니 추가에 실패하였습니다.") }
+            cartAddUseCase(dish.hash, currentAmount, dish.title)
+                .onSuccess { _isAddSuccess.emit(UiEvents.Success(it)) }
+                .onFailure {
+                    _isAddSuccess.emit(UiEvents.Error(it as ErrorEntity))
+                }
         }
     }
 }
