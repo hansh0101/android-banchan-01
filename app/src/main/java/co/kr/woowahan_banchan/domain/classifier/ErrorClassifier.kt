fb6a450cbcbba92ab69993170e95815f1bc1ec51
hansh0101 <hansh0101@naver.com>
2022-08-20 21:01:39 +0900

    [DEL] #105 - ErrorClassifier 인터페이스 및 구현체 제거

--- app/src/main/java/co/kr/woowahan_banchan/domain/classifier/ErrorClassifier.kt
+++
@@ -1,7 +0,0 @@
-package co.kr.woowahan_banchan.domain.classifier
-
-import co.kr.woowahan_banchan.domain.entity.error.ErrorEntity
-
-interface ErrorClassifier {
-    fun classifyError(throwable: Throwable): ErrorEntity
-}
