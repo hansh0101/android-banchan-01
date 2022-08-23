37e0269544078ca7134c3373c03cf99f8e339d45
hansh0101 <hansh0101@naver.com>
2022-08-23 18:09:24 +0900

    [FEATURE] #131 - DataSource Flow 예외처리 구현

--- app/src/main/java/co/kr/woowahan_banchan/data/datasource/local/order/OrderDataSourceImpl.kt
+++ app/src/main/java/co/kr/woowahan_banchan/data/datasource/local/order/OrderDataSourceImpl.kt
@@ -2,11 +2,14 @@ package co.kr.woowahan_banchan.data.datasource.local.order
 
 import co.kr.woowahan_banchan.data.database.dao.OrderDao
 import co.kr.woowahan_banchan.data.extension.runCatchingErrorEntity
+import co.kr.woowahan_banchan.data.extension.toErrorEntity
 import co.kr.woowahan_banchan.data.model.local.OrderDto
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.withContext
-import timber.log.Timber
 import javax.inject.Inject
 
 class OrderDataSourceImpl @Inject constructor(
@@ -24,8 +27,7 @@ class OrderDataSourceImpl @Inject constructor(
         orderDao.getLatestOrderTime()
             .filterNotNull()
             .catch { exception ->
-                Timber.e(exception)
-                emit(0)
+                throw exception.toErrorEntity()
             }.flowOn(coroutineDispatcher)
 
     override suspend fun getTime(orderId: Long): Result<Long> =
