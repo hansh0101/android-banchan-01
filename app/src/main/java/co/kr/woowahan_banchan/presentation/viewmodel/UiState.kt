328aa92461d9f0e3cc5fa0d7390ddac8d342d9a4
hansh0101 <hansh0101@naver.com>
2022-08-26 01:06:27 +0900

    [DEL] #186 - 미사용 클래스 제거

--- app/src/main/java/co/kr/woowahan_banchan/presentation/viewmodel/UiState.kt
+++
@@ -1,7 +0,0 @@
-package co.kr.woowahan_banchan.presentation.viewmodel
-
-sealed class UiState<out T> {
-    object Init : UiState<Nothing>()
-    data class Success<T>(val data: T) : UiState<T>()
-    data class Error(val message: String?) : UiState<Nothing>()
-}
