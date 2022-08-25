16c03766aa5dc80d740e002c7ad68282047a43cd
hansh0101 <hansh0101@naver.com>
2022-08-25 17:47:48 +0900

    [FEATURE] #175 - BestFragment UiStates, UiEvents 적용

--- app/src/main/java/co/kr/woowahan_banchan/presentation/viewmodel/main/BestViewModel.kt
+++ app/src/main/java/co/kr/woowahan_banchan/presentation/viewmodel/main/BestViewModel.kt
@@ -3,12 +3,14 @@ package co.kr.woowahan_banchan.presentation.viewmodel.main
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import co.kr.woowahan_banchan.domain.entity.dish.BestItem
+import co.kr.woowahan_banchan.domain.entity.error.ErrorEntity
 import co.kr.woowahan_banchan.domain.usecase.GetBestsUseCase
+import co.kr.woowahan_banchan.presentation.viewmodel.UiEvents
 import co.kr.woowahan_banchan.presentation.viewmodel.UiState
+import co.kr.woowahan_banchan.presentation.viewmodel.UiStates
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -16,16 +18,31 @@ import javax.inject.Inject
 class BestViewModel @Inject constructor(
     private val getBestsUseCase: GetBestsUseCase
 ) : ViewModel() {
-    private val _bestItems = MutableStateFlow<UiState<List<BestItem>>>(UiState.Init)
-    val bestItems: StateFlow<UiState<List<BestItem>>> get() = _bestItems
+    private val _bestItems = MutableStateFlow<UiStates<List<BestItem>>>(UiStates.Init)
+    val bestItems: StateFlow<UiStates<List<BestItem>>> get() = _bestItems
+    private val _bestItemsEvent = MutableSharedFlow<UiEvents<Unit>>()
+    val bestItemsEvent: SharedFlow<UiEvents<Unit>> get() = _bestItemsEvent
+    private var collectJob: Job? = null
 
-    fun getBests() {
-        viewModelScope.launch {
+    init {
+        getBests()
+    }
+
+    private fun getBests() {
+        collectJob = viewModelScope.launch {
             getBestsUseCase()
-                .catch { _bestItems.value = UiState.Error("상품을 불러오는 것에 실패하였습니다.") }
                 .collect { result ->
-                    result.onSuccess { _bestItems.value = UiState.Success(it) }
+                    result.onSuccess { _bestItems.value = UiStates.Success(it) }
+                        .onFailure {
+                            _bestItems.value = UiStates.Error(it.message)
+                            _bestItemsEvent.emit(UiEvents.Error(it as ErrorEntity))
+                        }
                 }
         }
+    }
+
+    fun reFetchBests() {
+        collectJob?.cancel()
+        getBests()
     }
 }
