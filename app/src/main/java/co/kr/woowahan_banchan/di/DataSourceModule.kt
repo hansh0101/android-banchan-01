c6ae305f26721000366259b8bf1dc0ae58ae4ba8
hansh0101 <hansh0101@naver.com>
2022-08-27 01:35:52 +0900

    [REFACTOR] #195 - DataSourceModule Provides -> Binds 수정

--- app/src/main/java/co/kr/woowahan_banchan/di/DataSourceModule.kt
+++ app/src/main/java/co/kr/woowahan_banchan/di/DataSourceModule.kt
@@ -1,10 +1,5 @@
 package co.kr.woowahan_banchan.di
 
-import co.kr.woowahan_banchan.data.api.*
-import co.kr.woowahan_banchan.data.database.dao.CartDao
-import co.kr.woowahan_banchan.data.database.dao.HistoryDao
-import co.kr.woowahan_banchan.data.database.dao.OrderDao
-import co.kr.woowahan_banchan.data.database.dao.OrderItemDao
 import co.kr.woowahan_banchan.data.datasource.local.cart.CartDataSource
 import co.kr.woowahan_banchan.data.datasource.local.cart.CartDataSourceImpl
 import co.kr.woowahan_banchan.data.datasource.local.history.HistoryDataSource
@@ -23,79 +18,69 @@ import co.kr.woowahan_banchan.data.datasource.remote.sidedish.SideDishDataSource
 import co.kr.woowahan_banchan.data.datasource.remote.sidedish.SideDishDataSourceImpl
 import co.kr.woowahan_banchan.data.datasource.remote.soupdish.SoupDishDataSource
 import co.kr.woowahan_banchan.data.datasource.remote.soupdish.SoupDishDataSourceImpl
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import kotlinx.coroutines.CoroutineDispatcher
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object DataSourceModule {
-    @Provides
+abstract class DataSourceModule {
+    @Binds
     @Singleton
-    fun provideCartDataSource(
-        cartDao: CartDao,
-        @IoDispatcher coroutineDispatcher: CoroutineDispatcher
-    ): CartDataSource = CartDataSourceImpl(cartDao, coroutineDispatcher)
+    abstract fun provideCartDataSource(
+        cartDataSourceImpl: CartDataSourceImpl
+    ): CartDataSource
 
-    @Provides
+    @Binds
     @Singleton
-    fun provideHistoryDataSource(
-        historyDao: HistoryDao,
-        @IoDispatcher coroutineDispatcher: CoroutineDispatcher
-    ): HistoryDataSource = HistoryDataSourceImpl(historyDao, coroutineDispatcher)
+    abstract fun provideHistoryDataSource(
+        historyDataSourceImpl: HistoryDataSourceImpl
+    ): HistoryDataSource
 
-    @Provides
+    @Binds
     @Singleton
-    fun provideOrderDataSource(
-        orderDao: OrderDao,
-        @IoDispatcher coroutineDispatcher: CoroutineDispatcher
-    ): OrderDataSource = OrderDataSourceImpl(orderDao, coroutineDispatcher)
+    abstract fun provideOrderDataSource(
+        orderDataSourceImpl: OrderDataSourceImpl
+    ): OrderDataSource
 
-    @Provides
+    @Binds
     @Singleton
-    fun provideOrderItemDataSource(
-        orderItemDao: OrderItemDao,
-        @IoDispatcher coroutineDispatcher: CoroutineDispatcher
-    ): OrderItemDataSource = OrderItemDataSourceImpl(orderItemDao, coroutineDispatcher)
+    abstract fun provideOrderItemDataSource(
+        orderItemDataSourceImpl: OrderItemDataSourceImpl
+    ): OrderItemDataSource
 
-    @Provides
+    @Binds
     @Singleton
-    fun provideBestDataSource(
-        service: BestService,
-        @IoDispatcher coroutineDispatcher: CoroutineDispatcher
-    ): BestDataSource = BestDataSourceImpl(service, coroutineDispatcher)
+    abstract fun provideBestDataSource(
+        bestDataSourceImpl: BestDataSourceImpl
+    ): BestDataSource
 
-    @Provides
+    @Binds
     @Singleton
-    fun provideDetailDataSource(
-        service: DetailService,
-        @IoDispatcher coroutineDispatcher: CoroutineDispatcher
-    ): DetailDataSource = DetailDataSourceImpl(service, coroutineDispatcher)
+    abstract fun provideDetailDataSource(
+        detailDataSourceImpl: DetailDataSourceImpl
+    ): DetailDataSource
 
 
-    @Provides
+    @Binds
     @Singleton
-    fun provideMainDishDataSource(
-        service: MainDishService,
-        @IoDispatcher coroutineDispatcher: CoroutineDispatcher
-    ): MainDishDataSource = MainDishDataSourceImpl(service, coroutineDispatcher)
+    abstract fun provideMainDishDataSource(
+        mainDishDataSourceImpl: MainDishDataSourceImpl
+    ): MainDishDataSource
 
 
-    @Provides
+    @Binds
     @Singleton
-    fun provideSideDishDataSource(
-        service: SideDishService,
-        @IoDispatcher coroutineDispatcher: CoroutineDispatcher
-    ): SideDishDataSource = SideDishDataSourceImpl(service, coroutineDispatcher)
+    abstract fun provideSideDishDataSource(
+        sideDishDataSourceImpl: SideDishDataSourceImpl
+    ): SideDishDataSource
 
 
-    @Provides
+    @Binds
     @Singleton
-    fun provideSoupDishDataSource(
-        service: SoupDishService,
-        @IoDispatcher coroutineDispatcher: CoroutineDispatcher
-    ): SoupDishDataSource = SoupDishDataSourceImpl(service, coroutineDispatcher)
+    abstract fun provideSoupDishDataSource(
+        soupDishDataSourceImpl: SoupDishDataSourceImpl
+    ): SoupDishDataSource
 }
