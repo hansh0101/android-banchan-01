bb18fc45f2856aaf1ae5b0c9a105645b7caa5df4
hansh0101 <hansh0101@naver.com>
2022-08-27 01:30:03 +0900

    [REFACTOR] #195 - RemoteDataSourceImpl @Inject 어노테이션 추가 및 코루틴 디스패처 어노테이션으로 명시

--- app/src/main/java/co/kr/woowahan_banchan/data/datasource/remote/best/BestDataSourceImpl.kt
+++ app/src/main/java/co/kr/woowahan_banchan/data/datasource/remote/best/BestDataSourceImpl.kt
@@ -3,12 +3,14 @@ package co.kr.woowahan_banchan.data.datasource.remote.best
 import co.kr.woowahan_banchan.data.api.BestService
 import co.kr.woowahan_banchan.data.extension.runCatchingErrorEntity
 import co.kr.woowahan_banchan.data.model.remote.response.BestResponse
+import co.kr.woowahan_banchan.di.IoDispatcher
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
-class BestDataSourceImpl(
+class BestDataSourceImpl @Inject constructor(
     private val service: BestService,
-    private val coroutineDispatcher: CoroutineDispatcher
+    @IoDispatcher private val coroutineDispatcher: CoroutineDispatcher
 ) : BestDataSource {
     override suspend fun getBests(): Result<List<BestResponse>> =
         withContext(coroutineDispatcher) {
--- app/src/main/java/co/kr/woowahan_banchan/data/datasource/remote/maindish/MainDishDataSourceImpl.kt
+++ app/src/main/java/co/kr/woowahan_banchan/data/datasource/remote/maindish/MainDishDataSourceImpl.kt
@@ -3,12 +3,14 @@ package co.kr.woowahan_banchan.data.datasource.remote.maindish
 import co.kr.woowahan_banchan.data.api.MainDishService
 import co.kr.woowahan_banchan.data.extension.runCatchingErrorEntity
 import co.kr.woowahan_banchan.data.model.remote.response.DishResponse
+import co.kr.woowahan_banchan.di.IoDispatcher
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
-class MainDishDataSourceImpl(
+class MainDishDataSourceImpl @Inject constructor(
     private val service: MainDishService,
-    private val coroutineDispatcher: CoroutineDispatcher
+    @IoDispatcher private val coroutineDispatcher: CoroutineDispatcher
 ) : MainDishDataSource {
     override suspend fun getMainDishes(): Result<List<DishResponse>> =
         withContext(coroutineDispatcher) {
