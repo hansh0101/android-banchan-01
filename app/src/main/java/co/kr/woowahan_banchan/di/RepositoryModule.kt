2882da1db7a7f7b186f0481aa80515d11fa83df5
hansh0101 <hansh0101@naver.com>
2022-08-27 01:35:59 +0900

    [REFACTOR] #195 - RepositoryModule Provides -> Binds 수정

--- app/src/main/java/co/kr/woowahan_banchan/di/RepositoryModule.kt
+++ app/src/main/java/co/kr/woowahan_banchan/di/RepositoryModule.kt
@@ -1,79 +1,43 @@
 package co.kr.woowahan_banchan.di
 
-import co.kr.woowahan_banchan.data.datasource.local.cart.CartDataSource
-import co.kr.woowahan_banchan.data.datasource.local.history.HistoryDataSource
-import co.kr.woowahan_banchan.data.datasource.local.order.OrderDataSource
-import co.kr.woowahan_banchan.data.datasource.local.orderitem.OrderItemDataSource
-import co.kr.woowahan_banchan.data.datasource.remote.best.BestDataSource
-import co.kr.woowahan_banchan.data.datasource.remote.detail.DetailDataSource
-import co.kr.woowahan_banchan.data.datasource.remote.maindish.MainDishDataSource
-import co.kr.woowahan_banchan.data.datasource.remote.sidedish.SideDishDataSource
-import co.kr.woowahan_banchan.data.datasource.remote.soupdish.SoupDishDataSource
 import co.kr.woowahan_banchan.data.repository.*
 import co.kr.woowahan_banchan.domain.repository.*
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import kotlinx.coroutines.CoroutineDispatcher
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object RepositoryModule {
-    @Provides
+abstract class RepositoryModule {
+    @Binds
     @Singleton
-    fun provideDishRepository(
-        cartDataSource: CartDataSource,
-        bestDataSource: BestDataSource,
-        mainDishDataSource: MainDishDataSource,
-        sideDishDataSource: SideDishDataSource,
-        soupDishDataSource: SoupDishDataSource,
-        @DefaultDispatcher coroutineDispatcher: CoroutineDispatcher
-    ): DishRepository = DishRepositoryImpl(
-        cartDataSource,
-        bestDataSource,
-        mainDishDataSource,
-        sideDishDataSource,
-        soupDishDataSource,
-        coroutineDispatcher
-    )
+    abstract fun provideDishRepository(
+        dishRepositoryImpl: DishRepositoryImpl
+    ): DishRepository
 
-    @Provides
+    @Binds
     @Singleton
-    fun provideHistoryRepository(
-        historyDataSource: HistoryDataSource,
-        cartDataSource: CartDataSource,
-        detailDataSource: DetailDataSource,
-        @DefaultDispatcher coroutineDispatcher: CoroutineDispatcher
-    ): HistoryRepository =
-        HistoryRepositoryImpl(
-            historyDataSource,
-            cartDataSource,
-            detailDataSource,
-            coroutineDispatcher
-        )
+    abstract fun provideHistoryRepository(
+        historyRepositoryImpl: HistoryRepositoryImpl
+    ): HistoryRepository
 
-    @Provides
+    @Binds
     @Singleton
-    fun provideDetailRepository(
-        detailDataSource: DetailDataSource
-    ): DetailRepository = DetailRepositoryImpl(detailDataSource)
+    abstract fun provideDetailRepository(
+        detailRepositoryImpl: DetailRepositoryImpl
+    ): DetailRepository
 
-    @Provides
+    @Binds
     @Singleton
-    fun provideOrderHistoryRepository(
-        orderDataSource: OrderDataSource,
-        orderItemDataSource: OrderItemDataSource,
-        @DefaultDispatcher coroutineDispatcher: CoroutineDispatcher
-    ): OrderHistoryRepository =
-        OrderHistoryRepositoryImpl(orderDataSource, orderItemDataSource, coroutineDispatcher)
+    abstract fun provideOrderHistoryRepository(
+        orderHistoryRepositoryImpl: OrderHistoryRepositoryImpl
+    ): OrderHistoryRepository
 
-    @Provides
+    @Binds
     @Singleton
-    fun provideCartRepository(
-        cartDataSource: CartDataSource,
-        detailDataSource: DetailDataSource,
-        @DefaultDispatcher coroutineDispatcher: CoroutineDispatcher
-    ): CartRepository = CartRepositoryImpl(cartDataSource, detailDataSource, coroutineDispatcher)
+    abstract fun provideCartRepository(
+        cartRepositoryImpl: CartRepositoryImpl
+    ): CartRepository
 }
